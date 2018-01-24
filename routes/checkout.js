cd8dc99de9aff4784c853a63b89367d54de91bba
Neil Gebbie <n_gebbie@live.com>
2018-01-24 18:06:29 +0000

    switch over to node

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var braintree = require('braintree');
+
+router.post('/', function(req, res, next) {
+  var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId: 'qkrxq8gwtxgt3s7j',
+    publicKey: '6ktpk49kx7sy28jn',
+    privateKey: '4cff104c0d294655d9a9429f33cc7198'
+  });
+
+  // Use the payment method nonce here
+  var nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  var newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, function(error, result) {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
